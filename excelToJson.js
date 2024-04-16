1136e51418a699fdcc42cdf95039c9f37c575ea1
Marco-Willian <marcowcandido@gmail.com>
2024-04-15 22:43:48 -0300

    Refactor import excel

--- excelToJson.js
+++ excelToJson.js
@@ -22,16 +22,16 @@ const categoriasFormatadas = [];
 
 for (let i = 0; i < json_data.length; i++) {
   const dados = json_data[i];
-
+  
   const alimentoFormatado = {
     grupo: dados['Grupo'],
     categoria_id: categorias[dados['Grupo']],
     nome: dados['Descrição do Alimento'],
-    carboidratos: parseFloat(dados['Carboidrato(g)']).toFixed(2),
-    proteinas: parseFloat(dados['Proteína(g)']).toFixed(2),
-    lipidios: parseFloat(dados['Lipídeos(g)']).toFixed(2),
-    calorias: parseFloat(dados['Energia(kcal)']).toFixed(2),
-    fibra_alimentar: parseFloat(dados['Fibra Alimentar(g)']).toFixed(2),
+    carboidratos: dados['Carboidrato(g)'] === 'NA' || dados['Carboidrato(g)'] === '*' || dados['Carboidrato(g)'] === 'Tr'  ? 0 : parseFloat(parseFloat(dados['Carboidrato(g)']).toFixed(2)),
+    proteinas: dados['Proteína(g)'] === 'NA' || dados['Proteína(g)'] === '*' || dados['Proteína(g)'] === 'Tr' ? 0 : parseFloat(parseFloat(dados['Proteína(g)']).toFixed(2)),
+    lipidios: dados['Lipídeos(g)'] === 'NA' || dados['Lipídeos(g)'] === '*' || dados['Lipídeos(g)'] === 'Tr' ? 0 : parseFloat(parseFloat(dados['Lipídeos(g)']).toFixed(2)),
+    calorias: dados['Energia(kcal)'] === 'NA' || dados['Energia(kcal)'] === '*' || dados['Energia(kcal)'] === 'Tr'  ? 0 : parseFloat(parseFloat(dados['Energia(kcal)']).toFixed(2)),
+    fibra_alimentar: dados['Fibra Alimentar(g)'] === 'NA' || dados['Fibra Alimentar(g)'] === '*' || dados['Fibra Alimentar(g)'] === 'Tr' ? 0 : parseFloat(parseFloat(dados['Fibra Alimentar(g)']).toFixed(2)),
     vitaminas: `Vitamina A: ${dados['Retinol(mcg)'] || 'NA'}, Vitamina C: ${dados['VitaminaC(mg)'] || 'NA'} , Vitamina B1: ${dados['Tiamina(mg)'] || 'NA'}, Vitamina B2: ${dados['Riboflavina(mg)'] || 'NA'}, Vitamina B6: ${dados['Piridoxina(mg)'] || 'NA'}, Vitamina B3: ${dados['Niacina(mg)'] || 'NA'}`,
     minerais: `Cálcio: ${dados['Calcio(mg)'] || 'NA'}, Magnésio: ${dados['Magnesio(mg)'] || 'NA'}, Manganês: ${dados['Manganes(mg)'] || 'NA'}, Fósforo: ${dados['Fosforo(mg)'] || 'NA'}, Ferro: ${dados['Ferro(mg)'] || 'NA'}, Sódio: ${dados['Sodio(mg)'] || 'NA'}, Potássio: ${dados['Potassio(mg)'] || 'NA'}, Cobre: ${dados['Cobre(mcg)'] || 'NA'}, Zinco: ${dados['Zinco(mg)'] || 'NA'}, Selênio: ${dados['Selenio(mcg)'] || 'NA'}`,
     createdAt: new Date(),
@@ -40,8 +40,9 @@ for (let i = 0; i < json_data.length; i++) {
   alimentosFormatados.push(alimentoFormatado);
   
   const categoriaFormatada = {
-    id: categorias[dados['Grupo']],
-    nome: dados['Grupo']
+    nome: dados['Grupo'],
+    createdAt: new Date(),
+    updatedAt: new Date()
   };
   categoriasFormatadas.push(categoriaFormatada);
 }
--- importData.js
+++ importData.js
@@ -3,52 +3,83 @@ const fs = require('fs');
 
 // Carregue os dados do arquivo JSON
 const rawDataCategorias = fs.readFileSync('./arquivo_base/categorias.json');
-const categoriasFormatadas = await JSON.parse(rawDataCategorias);
+const categoriasFormatadas = JSON.parse(rawDataCategorias);
 
 const rawDataAlimentos = fs.readFileSync('./arquivo_base/alimentos.json');
-const alimentosFormatados = await JSON.parse(rawDataAlimentos);
+const alimentosFormatados = JSON.parse(rawDataAlimentos);
 
-// Abra uma conexão com o banco de dados SQLite
-const db = new sqlite3.Database('./src/database/storage/database.sqlite');
+async function createTable(db) {
+  return new Promise((resolve, reject) => {
+    db.run(`CREATE TABLE IF NOT EXISTS alimentos (
+      id INTEGER PRIMARY KEY,
+      categoria_id INTEGER,
+      nome TEXT,
+      grupo TEXT,
+      carboidratos REAL,
+      proteinas REAL,
+      lipidios REAL,
+      calorias REAL,
+      fibra_alimentar REAL,
+      vitaminas TEXT,
+      minerais TEXT,
+      createdAt TEXT,
+      updatedAt TEXT
+    )`, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    })
+    db.run(`CREATE TABLE IF NOT EXISTS categorias (
+      id INTEGER PRIMARY KEY,
+      nome TEXT,
+      createdAt TEXT,
+      updatedAt TEXT
+    )`, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
-db.run(`CREATE TABLE IF NOT EXISTS categorias (
-  id INTEGER PRIMARY KEY,
-  nome TEXT
-)`);
+async function insertData(db, stmt, data) {
+  return new Promise((resolve, reject) => {
+    db.run(stmt, data, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
-// Crie uma tabela no banco de dados (se ainda não existir)
-db.run(`CREATE TABLE IF NOT EXISTS alimentos (
-  id INTEGER PRIMARY KEY,
-  categoria_id INTEGER,
-  grupo TEXT,
-  nome TEXT,
-  carboidratos REAL,
-  proteinas REAL,
-  lipidios REAL,
-  calorias REAL,
-  fibra_alimentar REAL,
-  vitaminas TEXT
-  minerais TEXT
-  createdAt TEXT,
-  updatedAt TEXT
-)`);
+async function importData() {
+  const db = new sqlite3.Database('./src/database/storage/database.sqlite');
+
+  try {
+    await createTable(db);
+
+    const stmtCategoria = 'INSERT INTO categorias (nome, createdAt, updatedAt) VALUES (?, ?, ?)';
+    for (const categoria of categoriasFormatadas) {
+      await insertData(db, stmtCategoria, [categoria.nome, categoria.createdAt, categoria.updatedAt]);
+    }
+
+    const stmtAlimento = 'INSERT INTO alimentos (categoria_id, nome, grupo, carboidratos, proteinas, lipidios, calorias, fibra_alimentar, vitaminas, minerais, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    for (const alimento of alimentosFormatados) {
+      await insertData(db, stmtAlimento, [alimento.categoria_id, alimento.nome, alimento.grupo, alimento.carboidratos, alimento.proteinas, alimento.lipidios, alimento.calorias, alimento.fibra_alimentar, alimento.vitaminas, alimento.minerais, alimento.createdAt, alimento.updatedAt]);
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    db.close();
+  }
+}
+
+importData();
 
 
-const stmtCategoria = db.prepare('INSERT INTO categorias (id, nome) VALUES (?, ?)');
-
-categoriasFormatadas.forEach((categoria) => {
-  stmtCategoria.run(categoria.id, categoria.nome);
-});
-
-// Insira os dados na tabela
-const stmt = db.prepare('INSERT INTO alimentos (categoria_id, nome, grupo, carboidratos, proteinas, lipidios, calorias, fibra_alimentar, vitaminas, minerais, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
-
-alimentosFormatados.forEach((alimento) => {
-  stmt.run(alimento.categoria_id, alimento.nome, alimento.grupo, alimento.carboidratos, alimento.proteinas, alimento.lipidios, alimento.calorias, alimento.fibra_alimentar, alimento.vitaminas, alimento.minerais, alimento.createdAt, alimento.updatedAt);
-});
-
-// Finalize o comando de inserção
-stmt.finalize();
-
-// Feche a conexão com o banco de dados
-db.close();
